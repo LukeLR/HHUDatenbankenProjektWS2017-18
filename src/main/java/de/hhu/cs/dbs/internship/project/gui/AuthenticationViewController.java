e9ef72505786b84b9c08cf26cc2ad9756a644924
Lukas <public@lrose.de>
2018-02-22 20:03:55 +0100

    Implemented login

--- src/main/java/de/hhu/cs/dbs/internship/project/gui/AuthenticationViewController.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/gui/AuthenticationViewController.java
@@ -2,8 +2,13 @@ package de.hhu.cs.dbs.internship.project.gui;
 
 import com.alexanderthelen.applicationkit.database.Data;
 
+import de.hhu.cs.dbs.internship.project.Project;
+
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Logger;
 
 public class AuthenticationViewController extends com.alexanderthelen.applicationkit.gui.AuthenticationViewController {
@@ -19,9 +24,32 @@ public class AuthenticationViewController extends com.alexanderthelen.applicatio
 
     @Override
     public void loginUser(Data data) throws SQLException {
+    	boolean authenticated = false;
+    	//TODO: Use finer log levels
+    	//TODO: Password hashing, PLEASE!
     	Logger logger = Logger.getLogger(this.getClass().getName() + " Login event");
+    	logger.info("Test");
     	logger.info("User tried to login: " + data.toString());
-        throw new SQLException(getClass().getName() + ".loginUser(Data) nicht implementiert.");
+    	
+    	PreparedStatement userAccountQuery = Project.getInstance().getConnection().prepareStatement("SELECT E_Mail_Adresse, Passwort FROM Kunde WHERE E_Mail_Adresse = ?");
+    	logger.info("SQL Statement for login information search: " + userAccountQuery.toString());
+    	userAccountQuery.setString(1, data.get("email").toString());
+        ResultSet userAccountResults = userAccountQuery.executeQuery();
+        logger.info("Found users:");
+        while (userAccountResults.next()) {
+        	String storedEmail = userAccountResults.getString("E_Mail_Adresse");
+        	String storedPassword = userAccountResults.getString("Passwort");
+        	String enteredPassword = data.get("password").toString();
+            logger.info("Found user with E-Mail: " + storedEmail + " and password: " + storedPassword + ", entered password was: " + enteredPassword);
+            if (storedPassword.equals(enteredPassword)) {
+            	 logger.info("User credentials correct. Done searching for users.");
+            	 authenticated = true;
+            }
+        }
+        if (!authenticated) {
+        	logger.warning("User credentials incorrect. Done searching for users.");
+        	throw new SQLException("Username or password incorrect!");
+        }
     }
 
     @Override
