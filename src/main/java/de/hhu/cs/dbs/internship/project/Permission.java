acf945d409751044174aa83d1b9c8b9266117c50
Lukas <public@lrose.de>
2018-02-23 20:27:59 +0100

    Implemented permissionLevelToString

--- src/main/java/de/hhu/cs/dbs/internship/project/Permission.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/Permission.java
@@ -1,9 +1,20 @@
 package de.hhu.cs.dbs.internship.project;
 
 public class Permission {
-	public static int UNAUTHORIZED = 0;
-	public static int READ_ONLY = 1;
-	public static int CUSTOMER = 2;
-	public static int PREMIUM_CUSTOMER = 3;
-	public static int SHOP_ASSISTANT = 4;
+	public static final int UNAUTHORIZED = 0;
+	public static final int READ_ONLY = 1;
+	public static final int CUSTOMER = 2;
+	public static final int PREMIUM_CUSTOMER = 3;
+	public static final int SHOP_ASSISTANT = 4;
+	
+	public static String permissionLevelToString (int permissionLevel) {
+		switch (permissionLevel) {
+			case UNAUTHORIZED: return "Unauthorized";
+			case READ_ONLY: return "Read-only";
+			case CUSTOMER: return "Customer";
+			case PREMIUM_CUSTOMER: return "Premium customer";
+			case SHOP_ASSISTANT: return "Shop assistant";
+			default: return "Undefined.";
+		}
+	}
 }
