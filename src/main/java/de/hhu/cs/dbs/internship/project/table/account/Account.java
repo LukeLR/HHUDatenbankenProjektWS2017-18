8ba27bdc08a8b1896c0101511872843f050c0fd6
Lukas <public@lrose.de>
2018-02-24 00:33:08 +0100

    first implementation of Account.updateRowWithData

--- src/main/java/de/hhu/cs/dbs/internship/project/table/account/Account.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/account/Account.java
@@ -40,7 +40,22 @@ public class Account extends Table {
 
     @Override
     public void updateRowWithData(Data data, Data data1) throws SQLException {
-        throw new SQLException(getClass().getName() + ".updateRowWithData(Data, Data) nicht implementiert.");
+    	Logger logger = Logger.getLogger(this.getClass().getName() + " Login event");
+    	logger.info("Trying to change account data from " + data + " to " + data1 + ".");
+    	String updateQuery = "UPDATE Kunde SET "
+    			+ "E_Mail_Adresse = " + data1.get("E-Mail-Adresse") + ", "
+    			+ "Passwort = " + data1.get("Passwort") + ", "
+    			+ "Vorname = " + data1.get("Vorname") + ", "
+    			+ "Nachname = " + data1.get("Nachname") + " "
+    			+ "WHERE E_Mail_Adresse = " + data.get("E-Mail-Adresse") + "; ";
+    	updateQuery += "UPDATE Adresse SET "
+    			+ "Strasse = " + data1.get("Straße") + ", "
+    			+ "Hausnummer = " + data1.get("Hausnummer") + ", "
+    			+ "PLZ = " + data1.get("PLZ") + ", "
+    			+ "Ort = " + data1.get("Ort") + " "
+    			+ "WHERE Adressen_ID IN (SELECT Adressen_ID FROM Kunde "
+    			+ "WHERE E_Mail_Adresse = " + data.get("E-Mail-Adresse") + ");";
+    	return updateQuery;
     }
 
     @Override
