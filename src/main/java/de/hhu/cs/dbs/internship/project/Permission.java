14198394518eb7508d2603be9f5ddde46254f8a2
Lukas <public@lrose.de>
2018-02-23 20:17:29 +0100

    Added permission level definition

---
+++ src/main/java/de/hhu/cs/dbs/internship/project/Permission.java
@@ -0,0 +1,9 @@
+package de.hhu.cs.dbs.internship.project;
+
+public class Permission {
+	public static int UNAUTHORIZED = 0;
+	public static int READ_ONLY = 1;
+	public static int CUSTOMER = 2;
+	public static int PREMIUM_CUSTOMER = 3;
+	public static int ADMINISTRATOR = 10;
+}
