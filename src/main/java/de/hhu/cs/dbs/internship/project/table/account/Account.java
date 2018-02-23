ec9f5f5733ca3e02f5327a05b49a6199a30d07db
Lukas <public@lrose.de>
2018-02-23 20:39:00 +0100

    First implementation of account table view

--- src/main/java/de/hhu/cs/dbs/internship/project/table/account/Account.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/account/Account.java
@@ -3,12 +3,15 @@ package de.hhu.cs.dbs.internship.project.table.account;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.Project;
+
 import java.sql.SQLException;
 
 public class Account extends Table {
     @Override
     public String getSelectQueryForTableWithFilter(String s) throws SQLException {
-        throw new SQLException(getClass().getName() + ".getSelectQueryForTableWithFilter(String) nicht implementiert.");
+    	String selectQuery = "SELECT * FROM Kunde WHERE E_Mail_Adresse = " + Project.getInstance().getData().get("email");
+        return selectQuery;
     }
 
     @Override
