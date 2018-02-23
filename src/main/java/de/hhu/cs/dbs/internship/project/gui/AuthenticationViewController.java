12235ed96f4c6767b2a761a8ead6396141f01e38
Lukas <public@lrose.de>
2018-02-23 19:47:54 +0100

    remove unused import

--- src/main/java/de/hhu/cs/dbs/internship/project/gui/AuthenticationViewController.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/gui/AuthenticationViewController.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
