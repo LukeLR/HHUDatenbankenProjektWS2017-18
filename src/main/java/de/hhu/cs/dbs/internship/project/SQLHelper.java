14a58a3bf913df3a356adfb06477811f02b7c564
Lukas <public@lrose.de>
2018-02-24 14:09:29 +0100

    Moved code to helper function

--- src/main/java/de/hhu/cs/dbs/internship/project/SQLHelper.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/SQLHelper.java
@@ -8,7 +8,7 @@ import com.alexanderthelen.applicationkit.database.Connection;
 
 public class SQLHelper {
 	public static int getAddressIDByAddress
-		(String street, String houseNumber, String zipCode, String city, Connection con) throws SQLException
+	(String street, String houseNumber, String zipCode, String city, Connection con) throws SQLException
 	{
 		PreparedStatement addressRequestQuery = con.prepareStatement(
 				"SELECT Adressen_ID FROM Adresse "
@@ -28,5 +28,36 @@ public class SQLHelper {
 	{
 		return getAddressIDByAddress(street, houseNumber, zipCode, city,
 				Project.getInstance().getConnection());
+	}
+	
+	public static int getAddressIDWithChangedAddress
+	(String streetOld, String houseNumberOld, String zipCodeOld, String cityOld,
+	String streetNew, String houseNumberNew, String zipCodeNew, String cityNew, Connection con) throws SQLException
+	{
+		//Check if address has changed
+    	if (!streetOld.equals(streetNew) || !houseNumberOld.equals(houseNumberNew) || !zipCodeOld.equals(zipCodeNew)
+    		|| !cityOld.equals(cityNew))
+    	{
+    		//If address has changed: insert new address into table.
+    		//TODO: Maybe delete old address?
+    		PreparedStatement insertAddressStatement = con.prepareStatement(
+    				"INSERT INTO Adresse (Strasse, Hausnummer, PLZ, Ort, Adressen_ID) "
+    				+ "VALUES (?, ?, ?, ?, NULL)");
+    		insertAddressStatement.setString(1, streetNew);
+    		insertAddressStatement.setString(2, houseNumberNew);
+    		insertAddressStatement.setString(3, zipCodeNew);
+    		insertAddressStatement.setString(4, cityNew);
+    		
+    		insertAddressStatement.executeUpdate();
+    	}
+    	return getAddressIDByAddress (streetNew, houseNumberNew, zipCodeNew, cityNew);
+	}
+	
+	public static int getAddressIDWithChangedAddress
+	(String streetOld, String houseNumberOld, String zipCodeOld, String cityOld,
+	String streetNew, String houseNumberNew, String zipCodeNew, String cityNew) throws SQLException
+	{
+		return getAddressIDWithChangedAddress (streetOld, houseNumberOld, zipCodeOld, cityOld,
+				streetNew, houseNumberNew, zipCodeNew, cityNew, Project.getInstance().getConnection());
 	}
 }
