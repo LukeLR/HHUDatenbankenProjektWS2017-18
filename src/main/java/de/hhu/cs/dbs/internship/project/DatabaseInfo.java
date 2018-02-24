6c782c340bdf42d4c99c635802fdac0d8198f1ce
Lukas <public@lrose.de>
2018-02-24 18:58:53 +0100

    Updated DatabaseInfo tablename order

--- src/main/java/de/hhu/cs/dbs/internship/project/DatabaseInfo.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/DatabaseInfo.java
@@ -2,7 +2,7 @@ package de.hhu.cs.dbs.internship.project;
 
 public class DatabaseInfo {
 	public static final String[] TABLES_WITH_E_MAIL_ADDRESS =
-		{"Kunde", "Premiumkunde", "Angestellter", "Warenkorb", "Newsletter", "Newsletterabo"};
+		{"Premiumkunde", "Angestellter", "Warenkorb", "Newsletter", "Newsletterabo", "Kunde"};
 	public static final String[] TABLES_WITH_E_MAIL_ADDRESS_WITHOUT_KUNDE =
 		{"Premiumkunde", "Angestellter", "Warenkorb", "Newsletter", "Newsletterabo"};
 }
