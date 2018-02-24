50e173f258fcf60408a10405d210e4b975a5ad36
Lukas <public@lrose.de>
2018-02-24 21:58:42 +0100

    Started implementation of anbieter Table

--- src/main/java/de/hhu/cs/dbs/internship/project/table/angebot/Anbieter.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/angebot/Anbieter.java
@@ -7,10 +7,6 @@ import com.alexanderthelen.applicationkit.database.Table;
 
 public class Anbieter extends Table {
 
-	public Anbieter() {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
 		// TODO Auto-generated method stub
