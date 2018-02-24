c9bf67e548841e84f2506a79abb904f33fc7024a
Lukas <public@lrose.de>
2018-02-24 19:41:31 +0100

    Created stub table classes

---
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/angebot/Anbieter.java
@@ -0,0 +1,44 @@
+package de.hhu.cs.dbs.internship.project.table.angebot;
+
+import java.sql.SQLException;
+
+import com.alexanderthelen.applicationkit.database.Data;
+import com.alexanderthelen.applicationkit.database.Table;
+
+public class Anbieter extends Table {
+
+	public Anbieter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSelectQueryForRowWithData(Data data) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertRowWithData(Data data) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateRowWithData(Data oldData, Data newData) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteRowWithData(Data data) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/warenkorb/Warenkoerbe.java
@@ -0,0 +1,44 @@
+package de.hhu.cs.dbs.internship.project.table.warenkorb;
+
+import java.sql.SQLException;
+
+import com.alexanderthelen.applicationkit.database.Data;
+import com.alexanderthelen.applicationkit.database.Table;
+
+public class Warenkoerbe extends Table {
+
+	public Warenkoerbe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSelectQueryForRowWithData(Data data) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertRowWithData(Data data) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateRowWithData(Data oldData, Data newData) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteRowWithData(Data data) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
