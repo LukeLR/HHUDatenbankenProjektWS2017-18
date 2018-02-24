97b5adb1f1821341f73f6faa720b6d89dd66c41f
Lukas <public@lrose.de>
2018-02-24 19:47:38 +0100

    Implementing AlleAccounts table

--- src/main/java/de/hhu/cs/dbs/internship/project/table/account/AlleAccounts.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/account/AlleAccounts.java
@@ -5,22 +5,28 @@ import java.sql.SQLException;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
-public class AlleAccounts extends Table {
+import de.hhu.cs.dbs.internship.project.Project;
 
-	public AlleAccounts() {
-		// TODO Auto-generated constructor stub
-	}
+public class AlleAccounts extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String selectQuery = "SELECT E_Mail_Adresse AS 'E-Mail-Adresse', Vorname, Nachname, "
+    			+ "Strasse AS 'Straße', Hausnummer, PLZ, Ort FROM Kunde JOIN Adresse "
+    			+ "ON Kunde.Adressen_ID = Adresse.Adressen_ID";
+		if (filter != null && !filter.isEmpty()) {
+			selectQuery += " AND Kunde.E_Mail_Adresse LIKE '%" + filter + "'";
+		}
+        return selectQuery;
 	}
 
 	@Override
 	public String getSelectQueryForRowWithData(Data data) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String selectQuery = "SELECT E_Mail_Adresse AS 'E-Mail-Adresse', Vorname, Nachname, "
+    			+ "Strasse AS 'Straße', Hausnummer, PLZ, Ort FROM Kunde JOIN Adresse "
+    			+ "ON Kunde.Adressen_ID = Adresse.Adressen_ID WHERE E_Mail_Adresse = '"
+    			+ data.get("Kunde.E-Mail-Adresse") + "'";
+        return selectQuery;
 	}
 
 	@Override
