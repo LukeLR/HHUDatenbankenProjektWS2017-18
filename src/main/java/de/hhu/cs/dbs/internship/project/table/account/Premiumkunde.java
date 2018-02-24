7271e52e60b81c5d90cf5b095848315a44160969
Lukas <public@lrose.de>
2018-02-24 22:35:57 +0100

    Implement insert Premiumkunde

--- src/main/java/de/hhu/cs/dbs/internship/project/table/account/Premiumkunde.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/account/Premiumkunde.java
@@ -1,10 +1,13 @@
 package de.hhu.cs.dbs.internship.project.table.account;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
+
+import de.hhu.cs.dbs.internship.project.Project;
 
 public class Premiumkunde extends Table {
 
@@ -27,14 +30,24 @@ public class Premiumkunde extends Table {
 		Logger logger = Logger.getLogger(this.getClass().getName());
 		logger.info("Showing " + this.getClass().getName() + " for Data " + data.toString());
 		String selectQuery = "SELECT * FROM Premiumkunde "
-				+ "WHERE E_Mail_Adresse = '" + data.get("Kunde.E-Mail-Adresse") + "'";
+				+ "WHERE E_Mail_Adresse = '" + data.get("Kunde.E_Mail_Adresse") + "'";
 		return selectQuery;
 	}
 
 	@Override
 	public void insertRowWithData(Data data) throws SQLException {
-		// TODO Auto-generated method stub
-
+		Logger logger = Logger.getLogger(this.getClass().getName());
+		logger.info("Trying to insert new Dataset with data: " + data.toString());
+		
+		PreparedStatement insertPremiumkundeStatement = Project.getInstance().getConnection().prepareStatement(
+				"INSERT INTO Premiumkunde (Ablaufdatum, Studierendenausweis, E_Mail_Adresse) "
+				+ "VALUES (?, ?, ?)");
+		insertPremiumkundeStatement.setString(1, data.get("Ablaufdatum").toString());
+		insertPremiumkundeStatement.setString(2, data.get("Studierendenausweis").toString());
+		insertPremiumkundeStatement.setString(3, data.get("E_Mail_Adresse").toString());
+		insertPremiumkundeStatement.executeUpdate();
+		
+		logger.info("Inserted Premiumkunde data!");
 	}
 
 	@Override
