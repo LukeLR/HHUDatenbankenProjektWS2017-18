3c7eca3e13741044da7ae702952ad67f2d6c54ff
Lukas <public@lrose.de>
2018-02-25 18:03:17 +0100

    Created UnifiedLoggingHelper

---
+++ src/main/java/de/hhu/cs/dbs/internship/project/helpers/UnifiedLoggingHelper.java
@@ -0,0 +1,52 @@
+package de.hhu.cs.dbs.internship.project.helpers;
+
+import java.util.logging.Logger;
+
+import com.alexanderthelen.applicationkit.database.Data;
+
+public class UnifiedLoggingHelper {
+	public static void logShow(String tableName) {
+		Logger logger = Logger.getLogger(tableName);
+		logger.info("Showing " + tableName + ".");
+	}
+	
+	public static void logFilter(String tableName, String filter) {
+		Logger logger = Logger.getLogger(tableName);
+		logger.info("Searching for " + filter + " in " + tableName + " table.");
+	}
+	
+	public static void logSelect(String tableName, Data data) {
+		Logger logger = Logger.getLogger(tableName);
+		logger.info("Trying to get Data for Dataset " + data.toString() + " in table " + tableName + ".");
+	}
+	
+	public static void logInsert(String tableName, Data data) {
+		Logger logger = Logger.getLogger(tableName);
+		logger.info("Trying to insert new Dataset with data: " + data.toString() + " in table " + tableName + ".");
+	}
+	
+	public static void logInsertDone(String tableName, Data data, String key) {
+		Logger logger = Logger.getLogger(tableName);
+		logger.info("Dataset " + key + " inserted into " + tableName + "!");
+	}
+	
+	public static void logUpdate(String tableName, Data oldData, Data newData) {
+		Logger logger = Logger.getLogger(tableName);
+		logger.info("Trying to change " + tableName + " data from " + oldData + " to " + newData + ".");
+	}
+	
+	public static void logUpdateDone(String tableName, Data oldData, Data newData, String key) {
+		Logger logger = Logger.getLogger(tableName);
+		logger.info("Done changing " + tableName + " data for " + key + ".");
+	}
+	
+	public static void logDelete(String tableName, Data data) {
+		Logger logger = Logger.getLogger(tableName);
+		logger.info("Trying to delete Dataset from " + tableName + " with data: " + data.toString());
+	}
+	
+	public static void logDeleteDone(String tableName, Data data, String key) {
+		Logger logger = Logger.getLogger(tableName);
+		logger.info("Dataset for " + key + " deleted from " + tableName + ".");
+	}
+}
