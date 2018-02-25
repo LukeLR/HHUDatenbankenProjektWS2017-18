c1f4d486817538e2b2f746310974253d7ac437e1
Lukas <public@lrose.de>
2018-02-25 01:16:44 +0100

    Implement showing of Angestellter table

--- src/main/java/de/hhu/cs/dbs/internship/project/table/account/Angestellter.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/account/Angestellter.java
@@ -1,6 +1,7 @@
 package de.hhu.cs.dbs.internship.project.table.account;
 
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
@@ -9,8 +10,16 @@ public class Angestellter extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Logger logger = Logger.getLogger(this.getClass().getName());
+		logger.info("Showing " + this.getClass().getName());
+		String selectQuery = "SELECT * FROM (SELECT Jobbezeichnung, Gehalt, Kunde.E_Mail_Adresse, "
+				+ "Vorname, Nachname, Passwort, Adressen_ID AS 'AID' FROM Angestellter "
+				+ "JOIN Kunde ON Angestellter.E_Mail_Adresse = Kunde.E_Mail_Adresse) "
+				+ "JOIN Adresse ON AID = Adresse.Adressen_ID";
+		if (filter != null && !filter.isEmpty()) {
+			selectQuery += " AND Kunde.E_Mail_Adresse LIKE '%" + filter + "'";
+		}
+		return selectQuery;
 	}
 
 	@Override
