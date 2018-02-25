5451524b74f121fa837227cccac5f5c2e08abb85
Lukas <public@lrose.de>
2018-02-25 03:44:34 +0100

    Implemented show for Artikel

--- src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/Artikel.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/Artikel.java
@@ -1,6 +1,7 @@
 package de.hhu.cs.dbs.internship.project.table.artikel;
 
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
@@ -9,8 +10,16 @@ public class Artikel extends Table {
 	
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Logger logger = Logger.getLogger(this.getClass().getName());
+		logger.info("Showing " + this.getClass().getName());
+		
+		String selectQuery = "SELECT Bezeichnung, Beschreibung, Bild, Artikel_ID FROM Artikel";
+		if (filter != null && !filter.isEmpty()) {
+			logger.info("Searching for " + filter + " in " + this.getClass().getName() + " table");
+			selectQuery += " WHERE Bezeichnung LIKE '%" + filter + "%' "
+					+ "OR Beschreibung LIKE '%" + filter + "%'";
+		}
+		return selectQuery;
 	}
 
 	@Override
