c6f120dd25d5313e493a733446df2512214b43cd
Lukas <public@lrose.de>
2018-02-25 03:39:53 +0100

    Removed constructor from Angebot

--- src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/Angebot.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/Angebot.java
@@ -7,19 +7,7 @@ import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
 public class Angebot extends Table {
-
-	public Angebot() {
-		Logger logger = Logger.getLogger(this.getClass().getName());
-		logger.info("Showing " + this.getClass().getName());
-		
-		/*String selectQuery = "SELECT (Angebots_ID, Artikel_ID, Preis) FROM Angebot";
-		if (filter != null && !filter.isEmpty()) {
-			logger.info("Searching for " + filter + " in " + this.getClass().getName() + " table");
-			selectQuery += " WHERE Anbieterbezeichnung LIKE '%" + filter + "%'";
-		}
-		return selectQuery;*/
-	}
-
+	
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
 		// TODO Auto-generated method stub
