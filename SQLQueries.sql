31289ac30455b047daa095d12802c81d7d4c4794
Lukas <public@lrose.de>
2018-02-26 02:42:21 +0100

    Added SQLQueries.sql file

---
+++ SQLQueries.sql
@@ -0,0 +1,23 @@
+--Show-Artikel_empfielt_Artikel-Query
+SELECT
+    'Artikel1-Bezeichnung',
+    'Artikel1-Beschreibung',
+    'Artikel1-Bild',
+    'Artikel1-Artikel_ID',
+    'Artikel2-Artikel_ID',
+    Bezeichnung AS 'Artikel2-Bezeichnung',
+    Beschreibung AS 'Artikel2-Beschreibung',
+    Bild AS 'Artikel2-Bild'
+FROM (
+    SELECT
+        Bezeichnung AS 'Artikel1-Bezeichnung',
+        Beschreibung AS 'Artikel1-Beschreibung',
+        Bild AS 'Artikel1-Bild',
+        Artikel_ID1 AS 'Artikel1-Artikel_ID',
+        Artikel_ID2 AS 'Artikel2-Artikel_ID'
+    FROM Artikel_empfiehlt_Artikel
+    JOIN Artikel
+    ON Artikel_empfiehlt_Artikel.Artikel_ID1 = Artikel.Artikel_ID
+)
+JOIN Artikel
+ON 'Artikel2-Artikel_ID' = Artikel.Artikel_ID
--- src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/ArtikelempfiehltArtikel.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/ArtikelempfiehltArtikel.java
@@ -5,12 +5,36 @@ import java.sql.SQLException;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
+
 public class ArtikelempfiehltArtikel extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		UnifiedLoggingHelper.logShow(this.getClass().getName());
+		
+		String selectQuery =
+				"SELECT 'Artikel1-Bezeichnung', 'Artikel1-Beschreibung', 'Artikel1-Bild', "
+				+ "'Artikel1-Artikel_ID', 'Artikel2-Artikel_ID', "
+				+ "Bezeichnung AS 'Artikel2-Bezeichnung', "
+				+ "Beschreibung AS 'Artikel2-Beschreibung', "
+				+ "Bild AS 'Artikel2-Bild' FROM ("
+					+ "SELECT Bezeichnung AS 'Artikel1-Bezeichnung', "
+					+ "Beschreibung AS 'Artikel1-Beschreibung', "
+					+ "Bild AS 'Artikel1-Bild', "
+					+ "Artikel_ID1 AS 'Artikel1-Artikel_ID', "
+					+ "Artikel_ID2 AS 'Artikel2-Artikel_ID' "
+					+ "FROM Artikel_empfiehlt_Artikel JOIN Artikel "
+					+ "ON Artikel_empfiehlt_Artikel.Artikel_ID1 = Artikel.Artikel_ID) "
+				+ "JOIN Artikel ON 'Artikel2-Artikel_ID' = Artikel.Artikel_ID";
+		
+		if (filter != null && !filter.isEmpty()) {
+			UnifiedLoggingHelper.logFilter(this.getClass().getName(), filter);
+			selectQuery += " WHERE Artikel1-Beschreibung LIKE '%" + filter + "%'";
+		}
+		
+		UnifiedLoggingHelper.logShowDone(this.getClass().getName(), selectQuery);
+		return selectQuery;
 	}
 
 	@Override
