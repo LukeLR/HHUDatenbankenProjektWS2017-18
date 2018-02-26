32dc3df5362c01706a07dca6c1948ebff4687c56
Lukas <public@lrose.de>
2018-02-26 02:19:46 +0100

    Implement insert, update and delete on Angebot

--- src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/Angebot.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/Angebot.java
@@ -1,11 +1,13 @@
 package de.hhu.cs.dbs.internship.project.table.artikel;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.Project;
 import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
 
 public class Angebot extends Table {
@@ -35,20 +37,42 @@ public class Angebot extends Table {
 
 	@Override
 	public void insertRowWithData(Data data) throws SQLException {
-		// TODO Auto-generated method stub
-
+		UnifiedLoggingHelper.logInsert(this.getClass().getName(), data);
+		
+		PreparedStatement insertAngebotStatement = Project.getInstance().getConnection().prepareStatement(
+				"INSERT INTO Angebot (Angebots_ID, Artikel_ID, Preis) VALUES (?, NULL, ?)");
+		insertAngebotStatement.setInt(1, Integer.valueOf(String.valueOf(data.get("Angebot.Artikel_ID"))));
+		//TODO: Check for prices with decimals
+		insertAngebotStatement.setFloat(2, Float.valueOf(String.valueOf(data.get("Angebot.Preis"))));
+		insertAngebotStatement.executeUpdate();
+		
+		UnifiedLoggingHelper.logInsertDone(this.getClass().getName(), data, String.valueOf(data.get("Angebot.Artikel_ID")));
 	}
 
 	@Override
 	public void updateRowWithData(Data oldData, Data newData) throws SQLException {
-		// TODO Auto-generated method stub
-
+		UnifiedLoggingHelper.logUpdate(this.getClass().getName(), oldData, newData);
+		
+		PreparedStatement updateAngebotStatement = Project.getInstance().getConnection().prepareStatement(
+				"UPDATE Angebot SET Artikel_ID = ?, Preis = ? WHERE Angebots_ID = ?");
+		updateAngebotStatement.setInt(1, Integer.valueOf(String.valueOf(newData.get("Angebot.Artikel_ID"))));
+		updateAngebotStatement.setLong(2, Long.valueOf(String.valueOf(newData.get("Angebot.Preis"))));
+		updateAngebotStatement.setInt(3, Integer.valueOf(String.valueOf(oldData.get("Angebot.Angebots_ID"))));
+		updateAngebotStatement.executeUpdate();
+		
+		UnifiedLoggingHelper.logUpdateDone(this.getClass().getName(), oldData, newData, String.valueOf(oldData.get("Angebot.Angebots_ID")));
 	}
 
 	@Override
 	public void deleteRowWithData(Data data) throws SQLException {
-		// TODO Auto-generated method stub
-
+		UnifiedLoggingHelper.logDelete(this.getClass().getName(), data);
+		
+		PreparedStatement deleteAngebotStatement = Project.getInstance().getConnection().prepareStatement(
+				"DELETE FROM Angebot WHERE Angebots_ID = ?");
+		deleteAngebotStatement.setInt(1, Integer.valueOf(String.valueOf(data.get("Angebot.Angebots_ID"))));
+		deleteAngebotStatement.executeUpdate();
+		
+		UnifiedLoggingHelper.logDeleteDone(this.getClass().getName(), data, String.valueOf(data.get("Angebot.Angebots_ID")));
 	}
 
 }
