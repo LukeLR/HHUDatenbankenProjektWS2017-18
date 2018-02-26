3628478bc21470d42ee18fb8ecf64cdb16bd3252
Lukas <accounts@lrose.de>
2018-02-26 04:12:49 +0100

    Implemented AnbieterBietetAn show

--- src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/AnbieterBietetAn.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/AnbieterBietetAn.java
@@ -5,12 +5,39 @@ import java.sql.SQLException;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
+
 public class AnbieterBietetAn extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		UnifiedLoggingHelper.logShow(this.getClass().getName());
+		
+		String selectQuery =
+				"SELECT "
+					+ "Anbieter_bietet_an.Anbieterbezeichnung, "
+					+ "Anbieter_bietet_an.Angebots_ID, "
+					+ "Anbieter_bietet_an.Bestand, "
+					+ "Angebot.Angebots_ID, "
+					+ "Angebot.Artikel_ID, "
+					+ "Angebot.Preis, "
+					+ "Artikel.Bezeichnung, "
+					+ "Artikel.Beschreibung, "
+					+ "Artikel.Bild "
+				+ "FROM Anbieter_bietet_an "
+				+ "JOIN Angebot "
+				+ "ON Anbieter_bietet_an.Angebots_ID = Angebot.Angebots_ID "
+				+ "JOIN Artikel "
+				+ "ON Angebot.Artikel_ID = Artikel.Artikel_ID";
+		
+		if (filter != null && !filter.isEmpty()) {
+			UnifiedLoggingHelper.logFilter(this.getClass().getName(), filter);
+			selectQuery += " WHERE Artikel.Bezeichnung LIKE '%" + filter + "%' OR "
+					+ "Artikel.Beschreibung LIKE '%" + filter + "%'";
+		}
+		
+		UnifiedLoggingHelper.logShowDone(this.getClass().getName(), selectQuery);
+		return selectQuery;
 	}
 
 	@Override
