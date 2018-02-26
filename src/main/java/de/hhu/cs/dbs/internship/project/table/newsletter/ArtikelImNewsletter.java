80effa370059cbbb5e36b309d9446f9c3672f58a
Lukas <accounts@lrose.de>
2018-02-26 06:18:10 +0100

    Created ArtikelImNewsletter.java

---
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/newsletter/ArtikelImNewsletter.java
@@ -0,0 +1,40 @@
+package de.hhu.cs.dbs.internship.project.table.newsletter;
+
+import java.sql.SQLException;
+
+import com.alexanderthelen.applicationkit.database.Data;
+import com.alexanderthelen.applicationkit.database.Table;
+
+public class ArtikelImNewsletter extends Table {
+
+	@Override
+	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSelectQueryForRowWithData(Data data) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertRowWithData(Data data) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateRowWithData(Data oldData, Data newData) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteRowWithData(Data data) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
