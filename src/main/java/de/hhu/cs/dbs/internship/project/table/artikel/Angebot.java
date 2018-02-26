5712ffb85ca53a5826dccea38252055b2f916b22
Lukas <public@lrose.de>
2018-02-26 02:04:19 +0100

    Implement Show and Select in Angebot

--- src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/Angebot.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/artikel/Angebot.java
@@ -6,18 +6,31 @@ import java.util.logging.Logger;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
+
 public class Angebot extends Table {
 	
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		UnifiedLoggingHelper.logShow(this.getClass().getName());
+		
+		String selectQuery = "SELECT Bezeichnung, Beschreibung, Bild, Artikel.Artikel_ID, "
+				+ "Angebots_ID, Preis FROM Angebot JOIN Artikel "
+				+ "ON Angebot.Artikel_ID = Artikel.Artikel_ID";
+		if (filter != null && !filter.isEmpty()) {
+			UnifiedLoggingHelper.logFilter(this.getClass().getName(), filter);
+			selectQuery += " WHERE Bezeichnung LIKE '%" + filter + "%'";
+		}
+		return selectQuery;
 	}
 
 	@Override
 	public String getSelectQueryForRowWithData(Data data) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		UnifiedLoggingHelper.logSelect(this.getClass().getName(), data);
+		
+		String selectQuery = "SELECT Artikel_ID, Preis, Angebots_ID FROM Angebot "
+				+ "WHERE Angebots_ID = '" + String.valueOf(data.get("Angebot.Angebots_ID")) + "'";
+		return selectQuery;
 	}
 
 	@Override
