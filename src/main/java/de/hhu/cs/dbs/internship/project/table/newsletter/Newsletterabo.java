7ee980b2f0d22c69aeddcfa2f4c6ba3f41cd1e69
Lukas <accounts@lrose.de>
2018-02-26 05:20:03 +0100

    Implement Newsletterabo show

--- src/main/java/de/hhu/cs/dbs/internship/project/table/newsletter/Newsletterabo.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/newsletter/Newsletterabo.java
@@ -5,12 +5,31 @@ import java.sql.SQLException;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
+
 public class Newsletterabo extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		UnifiedLoggingHelper.logShow(this.getClass().getName());
+		
+		String selectQuery = "SELECT Newsletterabo.E_Mail_Adresse AS 'Abonnent', "
+					+ "Newsletterabo.Newsletter_ID, "
+					+ "Newsletter.Betreff, "
+					+ "Newsletter.Text, "
+					+ "Newsletter.Datum, "
+					+ "Newsletter.E_Mail_Adresse AS 'Herausgeber' "
+				+ "FROM Newsletterabo "
+				+ "JOIN Newsletter "
+				+ "ON Newsletterabo.Newsletter_ID = Newsletter.Newsletter_ID";
+		
+		if (filter != null && !filter.isEmpty()) {
+			UnifiedLoggingHelper.logFilter(this.getClass().getName(), filter);
+			selectQuery += " WHERE Newsletterabo.E_Mail_Adresse LIKE '%" + filter + "%'";
+		}
+		
+		UnifiedLoggingHelper.logShowDone(this.getClass().getName(), selectQuery);
+		return selectQuery;
 	}
 
 	@Override
