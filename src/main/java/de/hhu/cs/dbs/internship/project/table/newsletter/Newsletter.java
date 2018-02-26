b320c8fc5f2e742693d06c711a4f17bb6c023545
Lukas <accounts@lrose.de>
2018-02-26 04:58:07 +0100

    Implemented Newsletter Show

--- src/main/java/de/hhu/cs/dbs/internship/project/table/newsletter/Newsletter.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/newsletter/Newsletter.java
@@ -5,12 +5,24 @@ import java.sql.SQLException;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
+
 public class Newsletter extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		UnifiedLoggingHelper.logShow(this.getClass().getName());
+		
+		String selectQuery = "SELECT Betreff, Text, Newsletter_ID, E_Mail_Adresse "
+				+ "FROM Newsletter";
+		
+		if (filter != null && !filter.isEmpty()) {
+			UnifiedLoggingHelper.logFilter(this.getClass().getName(), filter);
+			selectQuery += " WHERE Betreff LIKE '%" + filter + "%'";
+		}
+		
+		UnifiedLoggingHelper.logShowDone(this.getClass().getName(), selectQuery);
+		return selectQuery;
 	}
 
 	@Override
