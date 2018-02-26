f4847227d884f895bbe2e9d6b3d90cbe0bad2a81
Lukas <accounts@lrose.de>
2018-02-26 06:02:02 +0100

    Implemented Artikel_gehoert_zu_Schlagwort insert

--- src/main/java/de/hhu/cs/dbs/internship/project/table/schlagwort/ArtikelGehoertZuSchlagwort.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/schlagwort/ArtikelGehoertZuSchlagwort.java
@@ -1,10 +1,12 @@
 package de.hhu.cs.dbs.internship.project.table.schlagwort;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.Project;
 import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
 
 public class ArtikelGehoertZuSchlagwort extends Table {
@@ -40,8 +42,18 @@ public class ArtikelGehoertZuSchlagwort extends Table {
 
 	@Override
 	public void insertRowWithData(Data data) throws SQLException {
-		// TODO Auto-generated method stub
-
+		UnifiedLoggingHelper.logInsert(this.getClass().getName(), data);
+		
+		PreparedStatement insertArtikelSchlagwortStatement = Project.getInstance().getConnection().prepareStatement(
+				"INSERT INTO Artikel_gehort_zu_Schlagwort (Artikel_ID, Schlagwort) "
+				+ "VALUES (?, ?)");
+		insertArtikelSchlagwortStatement.setInt(1, Integer.valueOf(String.valueOf("Artikel_gehoert_zu_Schlagwort.Artikel_ID")));
+		insertArtikelSchlagwortStatement.setString(2, String.valueOf(data.get("Artikel_gehoert_zu_Schlagwort.Schlagwort")));
+		insertArtikelSchlagwortStatement.executeUpdate();
+		
+		UnifiedLoggingHelper.logInsertDone(this.getClass().getName(), data,
+				String.valueOf("Artikel_gehoert_zu_Schlagwort.Artikel_ID") + "-"
+				+ String.valueOf(data.get("Artikel_gehoert_zu_Schlagwort.Schlagwort")));
 	}
 
 	@Override
