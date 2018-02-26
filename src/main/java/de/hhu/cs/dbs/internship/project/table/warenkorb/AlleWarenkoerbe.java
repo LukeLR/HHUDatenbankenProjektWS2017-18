87fbda7b69055cd108e4fe79e5434f6aea299f27
Lukas <accounts@lrose.de>
2018-02-26 06:15:27 +0100

    Implemented AlleWarenkoerbe show

--- src/main/java/de/hhu/cs/dbs/internship/project/table/warenkorb/AlleWarenkoerbe.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/warenkorb/AlleWarenkoerbe.java
@@ -5,12 +5,24 @@ import java.sql.SQLException;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
+
 public class AlleWarenkoerbe extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		UnifiedLoggingHelper.logShow(this.getClass().getName());
+		
+		String selectQuery = "SELECT Bestelldatum, Bestellstatus, Warenkorb_ID, E_Mail_Adresse, " + 
+				"Lieferdienst_Bezeichnung, Lieferdatum FROM Warenkorb";
+		
+		if (filter != null && !filter.isEmpty()) {
+			UnifiedLoggingHelper.logFilter(this.getClass().getName(), filter);
+			selectQuery += " WHERE E_Mail_Adresse LIKE '%" + filter + "%'";
+		}
+		
+		UnifiedLoggingHelper.logShowDone(this.getClass().getName(), selectQuery);
+		return selectQuery;
 	}
 
 	@Override
