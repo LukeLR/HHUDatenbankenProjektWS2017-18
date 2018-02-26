54e72e3b319174088b7f42f37b35455b65df8f74
Lukas <accounts@lrose.de>
2018-02-26 05:56:57 +0100

    Implemented ArtikelGehoertZuSchlagwort Show

--- src/main/java/de/hhu/cs/dbs/internship/project/table/schlagwort/ArtikelGehoertZuSchlagwort.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/schlagwort/ArtikelGehoertZuSchlagwort.java
@@ -5,12 +5,23 @@ import java.sql.SQLException;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
+
 public class ArtikelGehoertZuSchlagwort extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		UnifiedLoggingHelper.logShow(this.getClass().getName());
+		
+		String selectQuery = "SELECT Artikel_ID, Schlagwort FROM Artikel_gehort_zu_Schlagwort";
+		
+		if (filter != null && !filter.isEmpty()) {
+			UnifiedLoggingHelper.logFilter(this.getClass().getName(), filter);
+			selectQuery += " WHERE Schlagwort = '" + filter + "'";
+		}
+		
+		UnifiedLoggingHelper.logShowDone(this.getClass().getName(), selectQuery);
+		return selectQuery;
 	}
 
 	@Override
