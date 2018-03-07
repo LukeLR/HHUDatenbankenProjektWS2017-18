88e0c10fb8988065593eb5f510694cb27e8f9243
LukeLR <LukeLR@users.noreply.github.com>
2018-03-07 18:40:35 +0100

    Implemented ArtikelImNewsletter Show

--- src/main/java/de/hhu/cs/dbs/internship/project/table/newsletter/ArtikelImNewsletter.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/newsletter/ArtikelImNewsletter.java
@@ -5,12 +5,35 @@ import java.sql.SQLException;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
+
 public class ArtikelImNewsletter extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		UnifiedLoggingHelper.logShow(this.getClass().getName());
+		
+		String selectQuery = "SELECT "
+				+ "Artikel.Bezeichnung, "
+				+ "Artikel.Beschreibung, "
+				+ "Artikel.Bild, "
+				+ "Artikel_im_Newsletter.Artikel_ID, "
+				+ "Artikel_im_Newsletter.Newsletter_ID, "
+				+ "Newsletter.Betreff, "
+				+ "Newsletter.Datum, "
+				+ "Newsletter.E_Mail_Adresse AS Herausgeber "
+				+ "FROM Artikel_im_Newsletter "
+				+ "JOIN Artikel ON Artikel_im_Newsletter.Artikel_ID = Artikel.Artikel_ID "
+				+ "JOIN Newsletter ON Artikel_im_Newsletter.Newsletter_ID = Newsletter.Newsletter_ID";
+		
+		if (filter != null && !filter.isEmpty()) {
+			UnifiedLoggingHelper.logFilter(this.getClass().getName(), filter);
+			selectQuery += " WHERE Newsletter.E_Mail_Adresse LIKE '%" + filter + "%' OR"
+					+ " Artikel.Bezeichnung LIKE '%" + filter + "%'";
+		}
+		
+		UnifiedLoggingHelper.logShowDone(this.getClass().getName(), selectQuery);
+		return selectQuery;
 	}
 
 	@Override
