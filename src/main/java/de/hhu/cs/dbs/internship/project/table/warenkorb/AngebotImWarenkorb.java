ec3fa9f875b5c3e27c3ae46c6bf9bff471e42c58
LukeLR <LukeLR@users.noreply.github.com>
2018-03-08 02:46:22 +0100

    Implement AngebotImWarenkorb insert

--- src/main/java/de/hhu/cs/dbs/internship/project/table/warenkorb/AngebotImWarenkorb.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/warenkorb/AngebotImWarenkorb.java
@@ -1,10 +1,12 @@
 package de.hhu.cs.dbs.internship.project.table.warenkorb;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.Project;
 import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
 
 public class AngebotImWarenkorb extends Table {
@@ -62,8 +64,22 @@ public class AngebotImWarenkorb extends Table {
 
 	@Override
 	public void insertRowWithData(Data data) throws SQLException {
-		// TODO Auto-generated method stub
-
+		UnifiedLoggingHelper.logInsert(this.getClass().getName(), data);
+		
+		PreparedStatement insertAngebotImWarenkorbStatement = Project.getInstance().getConnection().prepareStatement(
+				"INSERT INTO Angebot_im_Warenkorb (Angebots_ID, Anbieterbezeichnung, Warenkorb_ID, Anzahl) "
+				+ "VALUES (?, ?, ?, ?)");
+		insertAngebotImWarenkorbStatement.setInt(1, Integer.valueOf(String.valueOf(data.get("Angebot_im_Warenkorb.Warenkorb_ID"))));
+		insertAngebotImWarenkorbStatement.setInt(2, Integer.valueOf(String.valueOf(data.get("Angebot_im_Warenkorb.Angebots_ID"))));
+		insertAngebotImWarenkorbStatement.setInt(3, Integer.valueOf(String.valueOf(data.get("Angebot_im_Warenkorb.Anbieterbezeichnung"))));
+		insertAngebotImWarenkorbStatement.setInt(4, Integer.valueOf(String.valueOf(data.get("Angebot_im_Warenkorb.Anzahl"))));
+		insertAngebotImWarenkorbStatement.executeUpdate();
+		
+		UnifiedLoggingHelper.logInsertDone(this.getClass().getName(), data,
+				String.valueOf(data.get("Angebot_im_Warenkorb.Warenkorb_ID")) + "-"
+				+ String.valueOf(data.get("Angebot_im_Warenkorb.Angebots_ID"))
+				+ String.valueOf(data.get("Angebot_im_Warenkorb.Anbieterbezeichnung")) + "-"
+				+ String.valueOf(data.get("Angebot_im_Warenkorb.Anzahl")));
 	}
 
 	@Override
