92dc565e8c71b2ec5ab11b9fd1be9f3b5dcbc3eb
LukeLR <LukeLR@users.noreply.github.com>
2018-03-08 03:24:05 +0100

    Implemented Warenkoerbe select

--- src/main/java/de/hhu/cs/dbs/internship/project/table/warenkorb/Warenkoerbe.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/warenkorb/Warenkoerbe.java
@@ -5,12 +5,30 @@ import java.sql.SQLException;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
+
 public class Warenkoerbe extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		UnifiedLoggingHelper.logShow(this.getClass().getName());
+		
+		String selectQuery = "SELECT Warenkorb.Warenkorb_ID, "
+				+ "Warenkorb.E_Mail_Adresse, "
+				+ "Warenkorb.Bestelldatum, "
+				+ "Warenkorb.Bestellstatus, "
+				+ "Warenkorb.Lieferdienst_Bezeichnung, "
+				+ "Warenkorb.Lieferdatum "
+				+ "FROM Warenkorb";
+		
+		if (filter != null && !filter.isEmpty()) {
+			UnifiedLoggingHelper.logFilter(this.getClass().getName(), filter);
+			selectQuery += " WHERE Warenkorb.E_Mail_Adresse LIKE '%" + filter + "%' OR "
+					+ "Warenkorb.Warenkorb_ID LIKE '%" + filter + "%'";
+		}
+		
+		UnifiedLoggingHelper.logShowDone(this.getClass().getName(), selectQuery);
+		return selectQuery;
 	}
 
 	@Override
