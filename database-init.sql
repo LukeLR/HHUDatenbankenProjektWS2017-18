694f13d0f22e844650500b8a29eead06ef321fa8
LukeLR <LukeLR@users.noreply.github.com>
2018-03-09 05:12:11 +0100

    SQL Documentation

--- database-init.sql
+++ database-init.sql
@@ -471,7 +471,11 @@ BEGIN
     WHERE E_Mail_Adresse = OLD.E_Mail_Adresse;*/
 END;
 
-
+/* Wenn ein Anbieter gelöscht werden muss, müssen auch alle Angebote,
+ * die dieser Anbieter anbietet, und deren Vorkommnisse in Warenkörben
+ * zunächst gelöscht werden, da sonst ein FOREIGN KEY CONSTRAINT-error
+ * droht.
+ */
 CREATE TRIGGER delete_anbieter
 BEFORE DELETE ON Anbieter
 BEGIN
