e4d600941c88c3adfc4af5727584d823f142adb0
LukeLR <LukeLR@users.noreply.github.com>
2018-03-12 02:48:35 +0100

    Permissions for ArtikelGehoertZuSchlagwort

--- src/main/java/de/hhu/cs/dbs/internship/project/table/schlagwort/ArtikelGehoertZuSchlagwort.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/schlagwort/ArtikelGehoertZuSchlagwort.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import com.alexanderthelen.applicationkit.database.Data;
 import com.alexanderthelen.applicationkit.database.Table;
 
+import de.hhu.cs.dbs.internship.project.Permission;
 import de.hhu.cs.dbs.internship.project.Project;
 import de.hhu.cs.dbs.internship.project.helpers.UnifiedLoggingHelper;
 
@@ -13,6 +14,7 @@ public class ArtikelGehoertZuSchlagwort extends Table {
 
 	@Override
 	public String getSelectQueryForTableWithFilter(String filter) throws SQLException {
+		Permission.hasSufficientPermission(Permission.READ_ONLY, this.getClass().getName());
 		UnifiedLoggingHelper.logShow(this.getClass().getName());
 		
 		String selectQuery = "SELECT Artikel_ID, Schlagwort FROM Artikel_gehoert_zu_Schlagwort";
@@ -28,6 +30,7 @@ public class ArtikelGehoertZuSchlagwort extends Table {
 
 	@Override
 	public String getSelectQueryForRowWithData(Data data) throws SQLException {
+		Permission.hasSufficientPermission(Permission.READ_ONLY, this.getClass().getName());
 		UnifiedLoggingHelper.logSelect(this.getClass().getName(), data);
 		
 		String selectQuery = "SELECT Artikel_ID, Schlagwort FROM Artikel_gehoert_zu_Schlagwort "
@@ -42,6 +45,7 @@ public class ArtikelGehoertZuSchlagwort extends Table {
 
 	@Override
 	public void insertRowWithData(Data data) throws SQLException {
+		Permission.hasSufficientPermission(Permission.SHOP_ASSISTANT, this.getClass().getName());
 		UnifiedLoggingHelper.logInsert(this.getClass().getName(), data);
 		
 		PreparedStatement insertArtikelSchlagwortStatement = Project.getInstance().getConnection().prepareStatement(
@@ -58,6 +62,7 @@ public class ArtikelGehoertZuSchlagwort extends Table {
 
 	@Override
 	public void updateRowWithData(Data oldData, Data newData) throws SQLException {
+		Permission.hasSufficientPermission(Permission.SHOP_ASSISTANT, this.getClass().getName());
 		UnifiedLoggingHelper.logUpdate(this.getClass().getName(), oldData, newData);
 		
 		PreparedStatement updateArtikelSchlagwortStatement = Project.getInstance().getConnection().prepareStatement(
@@ -79,6 +84,7 @@ public class ArtikelGehoertZuSchlagwort extends Table {
 
 	@Override
 	public void deleteRowWithData(Data data) throws SQLException {
+		Permission.hasSufficientPermission(Permission.SHOP_ASSISTANT, this.getClass().getName());
 		UnifiedLoggingHelper.logDelete(this.getClass().getName(), data);
 		
 		PreparedStatement deleteArtikelSchlagwortStatement = Project.getInstance().getConnection().prepareStatement(
