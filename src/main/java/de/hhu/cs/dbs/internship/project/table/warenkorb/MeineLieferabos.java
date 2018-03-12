c17642e4caf5f8ad93deaed7bec1b84bbdade502
LukeLR <LukeLR@users.noreply.github.com>
2018-03-12 08:45:46 +0100

    Updated MeineLieferabos select

--- src/main/java/de/hhu/cs/dbs/internship/project/table/warenkorb/MeineLieferabos.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/table/warenkorb/MeineLieferabos.java
@@ -47,7 +47,8 @@ public class MeineLieferabos extends Table {
 				+ "Lieferabo.Beginn, "
 				+ "Lieferabo.Ende "
 				+ "FROM Lieferabo "
-				+ "WHERE Lieferabo.Warenkorb_ID = '" + String.valueOf(data.get("Lieferabo.Warenkorb_ID")) + "'";
+				+ "WHERE Lieferabo.Warenkorb_ID = '"
+				+ String.valueOf(data.get("Lieferabo.Warenkorb_ID")) + "'";
 		
 		UnifiedLoggingHelper.logSelectDone(this.getClass().getName(), data, selectQuery);
 		return selectQuery;
