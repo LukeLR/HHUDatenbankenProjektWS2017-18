577037b94a54464bb63a15fe65ade5f4f36b66c0
LukeLR <LukeLR@users.noreply.github.com>
2018-03-12 01:08:31 +0100

    Merged functions

--- src/main/java/de/hhu/cs/dbs/internship/project/helpers/AddressIDHelper.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/helpers/AddressIDHelper.java
@@ -81,37 +81,7 @@ public class AddressIDHelper {
 	public static int getAddressIDByAddress
 	(String street, String houseNumber, int zipCode, String city, Connection con) throws SQLException
 	{
-		Logger logger = Logger.getLogger(AddressIDHelper.class.getName());
-		logger.info("Searching for address ID for address: " + street
-				+ " " + houseNumber + ", " + zipCode + " " + city);
-
-		PreparedStatement addressRequestQuery = con.prepareStatement(
-				"SELECT Adressen_ID FROM Adresse "
-						+ "WHERE Strasse = ? AND Hausnummer = ? AND PLZ = ? AND Ort = ?");
-
-		addressRequestQuery.setString(1, street);
-		addressRequestQuery.setString(2, houseNumber);
-		addressRequestQuery.setInt(3, zipCode);
-		addressRequestQuery.setString(4, city);
-
-		ResultSet addressResults = addressRequestQuery.executeQuery();
-		if (!addressResults.isClosed()) {
-			int addressID = addressResults.getInt("Adressen_ID");
-			logger.info("Address ID found: " + String.valueOf(addressID));
-			return addressID;
-		} else {
-			logger.info("Address not found! Inserting...");
-			PreparedStatement addressInsertQuery = con.prepareStatement(
-					"INSERT INTO Adresse (Strasse, Hausnummer, PLZ, Ort, Adressen_ID) "
-					+ "VALUES (?, ?, ?, ?, NULL)");
-			addressInsertQuery.setString(1, street);
-			addressInsertQuery.setString(2, houseNumber);
-			addressInsertQuery.setInt(3, zipCode);
-			addressInsertQuery.setString(4, city);
-			addressInsertQuery.executeUpdate();
-			// Get the Adressen_ID of the just inserted address.
-			return getAddressIDByAddress(street, houseNumber, zipCode, city, con);
-		}
+		return getAddressIDByAddress(street, houseNumber, zipCode, city, con, true);
 	}
 	/**
 	 * Searches the database for an address id by given address. In this case, no database connection is given, so the default one is used.
