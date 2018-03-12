1a8b0c00dd0703ca3e68183a1557b1379cdf65d2
LukeLR <LukeLR@users.noreply.github.com>
2018-03-12 01:51:16 +0100

    Refactor

--- src/main/java/de/hhu/cs/dbs/internship/project/Permission.java
+++ src/main/java/de/hhu/cs/dbs/internship/project/Permission.java
@@ -1,5 +1,11 @@
 package de.hhu.cs.dbs.internship.project;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import de.hhu.cs.dbs.internship.project.helpers.MiscHelpers;
+
 public class Permission {
 	public static final int UNAUTHORIZED = 0;
 	public static final int READ_ONLY = 1;
@@ -15,6 +21,27 @@ public class Permission {
 		case PREMIUM_CUSTOMER: return "Premium customer";
 		case SHOP_ASSISTANT: return "Shop assistant";
 		default: return "Undefined.";
+		}
+	}
+
+	public static boolean hasSufficientPermission (int requiredPermissionLevel, String parentClass) throws SQLException {
+		Logger logger = Logger.getLogger(MiscHelpers.class.getName());
+		int currentPermissionLevel = Integer.valueOf(String.valueOf(Project.getInstance().getData().get("permission")));
+		if (currentPermissionLevel >= requiredPermissionLevel) {
+			logger.info("Permissions ok for " + parentClass + ". Required: " + 
+					Permission.permissionLevelToString(requiredPermissionLevel) + " (" +
+					String.valueOf(requiredPermissionLevel) + "), user has: " + 
+					Permission.permissionLevelToString(currentPermissionLevel) + " (" + 
+					String.valueOf(currentPermissionLevel) + ").");
+			return true;
+		} else {
+			SQLException ex = new SQLException("Insufficient permissions! Required: " +
+					Permission.permissionLevelToString(requiredPermissionLevel) + " (" +
+					String.valueOf(requiredPermissionLevel) + "), user has: " + 
+					Permission.permissionLevelToString(currentPermissionLevel) + " (" + 
+					String.valueOf(currentPermissionLevel) + ").");
+			logger.log(Level.WARNING, "Insufficient permissions!", ex);
+			throw ex;
 		}
 	}
 }
